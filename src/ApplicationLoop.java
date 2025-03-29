a003f5e9ec510130274071f93a65548b454cee03
Kevin-TUS <count_curls@hotmail.com>
2025-03-29 02:55:28 +0000

    Minor changes

--- src/ApplicationLoop.java
+++ src/ApplicationLoop.java
@@ -1,11 +1,13 @@
 import Enums.Country;
 import Products.DiscontinuedProduct;
 import Products.PreOrderProduct;
+import Products.Product;
 import Sales.CountrySalesRecord;
 import Sales.ProductSalesBatch;
 import Sales.ProductSalesRecord;
 import com.sun.source.tree.Tree;
 
+import javax.swing.text.Keymap;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
@@ -42,23 +44,27 @@ public final class ApplicationLoop implements Runnable {
             , "First 10 products in sales batch [limit][for each]"
             , "Sorted revenue results by country [collect][group by enum][Tree map][sum double][sort by comparing lambda][foreach]"
             , "Top 5 countries by revenue [collect][Group by enum][Tree map][sum Double][sort][limit][foreach]"
-            , "Create Pre-order/discontinued product Partition Map [collect][partition by string contains][get TRUE][foreach]"
+            , "Show total revenue of each Pre-order and discontinued product [collect][partition by string contains][get TRUE][foreach]"
             , "Find any product with > €30k revenue [Map to Records][Group by product][Tree map][Sum Double][Filter][find any]"
             , "Show pre-order products by release date [Collect][Partition][Map to Product-LocalDateTime][Sort by date][Distinct][Output time duration]"
             , "Show discontinued products by discontinued date [Collect][Partition By][sorted Comparator][distinct][forEach]"
             , "Create sales record by country and output France's revenue [collect][group by][map]"
             , "Get count of countries with sales data [collect][GroupingBy][count]"
-            , "Get distinct product objects. Output Pre-order and discontinued products [map][distinct][sorted comparator][Switch pattern matching]"
+            , "Output Pre-order and discontinued products [map][distinct][sorted comparator][Switch pattern matching]"
             , "All match pre-order product [map][distinct][all match]"
             , "Any match discontinued product [map][distinct][any match]"
             , "None match product name [map][distinct][none match]"
             , "List all distinct products and their prices [map][distinct][sort][comparator][foreach]"
+            , "Output Country and Revenue map [Collect][toMap]"
 
 
     };
 
     public void run(){
         String lastInput = "";
+        output.accept("Welcome!");
+        listOptions();
+        boolean firstRun = true;
         while(isRunning){
             //DEMO - Enhanced Switch statement:
             try {
@@ -73,46 +79,25 @@ public final class ApplicationLoop implements Runnable {
                 }
 
                 switch (lastInput) {
-                    case "" -> {
-                        output.accept("Welcome!");
-                        output.accept("If you want to quit the application at any point, type in 'exit' to quit.");
-                        output.accept("Select an option:");
-                        listOptions();
-                    }
                     case "exit" -> {
                         isRunning = false;
                     }
                     case "1" -> { //"Lowest products sold [GroupBy String][Tree Map][Sum Int][Min]"
-                        if (lastestData != null) {
-                            //lastestData.filter(x -> x.getCountry() == Country.AT).forEach(System.out::println);
-
                             var res = lastestData.collect(Collectors.groupingBy(x -> x.getProduct().getProductName()
                                     , TreeMap::new
                                     , Collectors.summingInt(ProductSalesBatch::getQuantitySold)))
                                     .entrySet()
                                     .stream()
                                     .min(Comparator.comparing(x -> x.getValue()));
-
                             output.accept(res.get().getKey() + " has lowest sales with " + String.format("%,d",res.get().getValue()));
-                                    //.Entry<Product, Integer>
-                                    //.forEach((x, y) -> System.out.println(x + ": " + y + " sold."));
-
-                            //output.accept(lastestData.min(Comparator.comparing(ProductSalesBatch::getPrice)).toString());
-
-                        }
                     }
                     case "2" -> { //"Highest products sold [GroupBy String][Tree Map][Sum Int][Max]"
-                        if (lastestData != null) {
-                            //lastestData.filter(x -> x.getCountry() == Country.AT).forEach(System.out::println);
                             var res = lastestData.collect(Collectors.groupingBy(x -> x.getProduct().getProductName()
                                             , TreeMap::new
                                             , Collectors.summingInt(ProductSalesBatch::getQuantitySold)))
-                                    .entrySet()
-                                    .stream()
+                                    .entrySet().stream()
                                     .max(Comparator.comparing(x -> x.getValue()));
-
                             output.accept(res.get().getKey() + " has highest sales with " + String.format("%,d",res.get().getValue()));
-                        }
                     }
                     case "3" -> { //"Lowest revenue by country [GroupBy enum][TreeMap][Collectors Sum][min][comparing]"
                         var res = lastestData.collect(Collectors.groupingBy(x -> x.getCountry()
@@ -142,14 +127,11 @@ public final class ApplicationLoop implements Runnable {
 
                     }
                     case "6" -> { //"Sales revenue from Spain [filter by enum][Map to double][sum]"
-                        output.accept("Total sales revenue €" + String.format("%,.2f",
+                        output.accept("Total sales revenue for Spain €" + String.format("%,.2f",
                                 lastestData.filter(x -> x.getCountry() == Country.ES)
                                         .mapToDouble(ProductSalesBatch::getRevenue)
                                         .sum())
                         );
-
-                                //.collect(Collectors.groupingBy(x -> x.getProduct().getProductName(), TreeMap::new, Collectors.summingDouble(ProductSalesBatch::getRevenue)))
-                                //.forEach((x, y) -> System.out.println(String.format("€%,.2f",y) + " revenue from " + x));
                     }
                     case "7" -> { //"First iPhone sales result [filter by String][find first]"
                         output.accept(
@@ -161,15 +143,13 @@ public final class ApplicationLoop implements Runnable {
                         lastestData.limit(10).forEach(System.out::println);
                     }
                     case "9" -> { //"Sorted revenue results by country [collect][group by enum][Tree map][sum double][sort by comparing lambda][foreach]"
-                        if (lastestData != null) {
-                            lastestData.collect(Collectors
-                                    .groupingBy(ProductSalesBatch::getCountry
-                                            , TreeMap::new
-                                            , Collectors.summingDouble(ProductSalesBatch::getPrice)))
-                                    .entrySet().stream()
-                                    .sorted(Comparator.comparing(x -> x.getKey().name()))
-                                    .forEach(m -> System.out.println(m.getKey().toString() + ":"+ String.format(" €%,.2f", m.getValue())));
-                        }
+                        lastestData.collect(Collectors
+                                .groupingBy(ProductSalesBatch::getCountry
+                                        , TreeMap::new
+                                        , Collectors.summingDouble(ProductSalesBatch::getRevenue)))
+                                .entrySet().stream()
+                                .sorted(Comparator.comparing(x -> x.getKey().name()))
+                                .forEach(m -> System.out.println(m.getKey().toString() + ":"+ String.format(" €%,.2f", m.getValue())));
                     }
                     case "10" -> { //"Top 5 countries by revenue [collect][Group by enum][Tree map][sum Double][sort][limit][foreach]"
                         output.accept("Top 5 countries by revenue:");
@@ -184,12 +164,14 @@ public final class ApplicationLoop implements Runnable {
                                                 System.out.println( x.getKey().toString() + ": €" +String.format("%,.2f", x.getValue())));
 
                     }
-                    case "11" -> { //Create Pre-order/discontinued product Partition Map [collect][partition by string contains][get TRUE][foreach]
+                    case "11" -> { //Create Pre-order/discontinued product Partition Map [collect][partition by string contains][get TRUE][GroupBy][foreach]
                         lastestData.collect(Collectors.partitioningBy(
                                         x -> x.getProduct().getProductName().toLowerCase().contains("pre-order")
                                                 || x.getProduct().getProductName().toLowerCase().contains("discontinued")))
                                 .get(Boolean.TRUE)
-                                .forEach(System.out::println);
+                                .stream().collect(Collectors.groupingBy(x->x.getProduct().getProductName()
+                                        , TreeMap::new, Collectors.summingDouble(x->x.getRevenue())))
+                                .entrySet().stream().forEach(x->output.accept(x.getKey() +" made " + String.format("€%,.2f", x.getValue()) + " in revenue." ));;
                     }
                     case "12" -> {  //"Find any product with > €30k revenue [Map to Records][Group by product][Tree map][Sum Double][Filter][find any]"
                         var res = lastestData.map(entry -> new ProductSalesRecord(entry.getProduct().getProductName(), entry.getRevenue()))
@@ -201,9 +183,8 @@ public final class ApplicationLoop implements Runnable {
                                         .entrySet()
                                         .stream()
                                         .filter(x -> x.getValue() > 30_000)
-                                        .findAny().ifPresent(x-> System.out.println(x.getKey().toString() + ": "
+                                        .findAny().ifPresent(x-> System.out.println(x.getKey().toString() + " has a total revenue of "
                                                 + String.format("€%,.2f",x.getValue())));
-
                     }
                     case "13" -> { //Show pre-order products by release date [Collect][Partition][Map to Product-LocalDateTime][Sort by date][Distinct][Output time duration]
                         lastestData.collect(Collectors.partitioningBy(
@@ -260,23 +241,18 @@ public final class ApplicationLoop implements Runnable {
                                 .distinct()
                                 .sorted(Comparator.comparing(x->x.getProductName()))
                                 .toList();
-
                         for(Products.Product r : res){ //Product class is extended by DiscontinuedProduct and PreOrderProduct
                             switch(r){
                                 case Products.DiscontinuedProduct d ->{
-                                    output.accept("Pattern matching for DiscontinuedProduct: " + d.getProductName() + " discontinued on "
+                                    output.accept("Discontinued Product: " + d.getProductName() + " discontinued on "
                                             + d.getDiscontinuedDate().format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
                                 }
                                 case Products.PreOrderProduct p ->{
-                                    output.accept( "Pattern matching for PreOrderProduct: " + p.getProductName() + " releases on "
+                                    output.accept( "Pre-Order Product: " + p.getProductName() + " releases on "
                                             + p.getReleaseDate().format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
                                 }
-                                case null -> {
-                                    //Do nothing. Same as the default case but including the "null" case for demonstration
-                                }
-                                default -> {
-                                    //Do nothing
-                                }
+                                case null -> { /*Do nothing. Same as the default case but including the "null" case for demonstration*/ }
+                                default -> { /*Do nothing*/ }
                             }
                         }
                     }
@@ -312,9 +288,20 @@ public final class ApplicationLoop implements Runnable {
                                 .sorted(Comparator.comparing(x->x.getProductName()))
                                 .forEach(x->output.accept(x.getProductName() + " costs " + String.format("€%,.2f", x.getPrice())));
                     }
-
+                    case "22" -> { //Output unsorted Country and Total Revenue map [Collect][toMap]
+                        var map = lastestData.collect(Collectors.toMap(productSalesBatch -> productSalesBatch.getCountry()
+                                , productSalesBatch -> productSalesBatch.getRevenue(), (a, b) -> a + b));
+                        map.forEach((x,y)->output.accept(x + ": " + String.format("€%,.2f", y)));
+                    }
+                    case "" ->{
+                        if(!firstRun){
+                            listOptions();
+                            output.accept("Empty input was received. Please try again");
+                        }
+                        firstRun = false;
+                    }
                     default -> {
-                        //lastInput = getNextInput("Input '" + lastInput+ "' not recognised. Please try again");
+                        listOptions();
                         output.accept("Input '" + lastInput+ "' not recognised. Please try again");
                     }
                 }
@@ -336,10 +323,11 @@ public final class ApplicationLoop implements Runnable {
     }
 
     private void listOptions(){
+        output.accept("Select an option:");
         for (int i = 0; i < options.length; i++) {
             output.accept((i + 1) + ": " + options[i]);
         }
-
+        output.accept("If you want to quit the application at any point, type in 'exit' to quit.");
     }
 
     private String getNextInput(String message){
